5fba131f486fb6be91af5a631db478f2d53eb6b3
Bharadwaj <haribalbharadwaj@gmail.com>
2024-10-03 06:40:50 +0530

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,34 @@
+const filters = [
+  {
+    name: "All",
+    imageUrl:
+      "https://imgs.search.brave.com/Y06oLSiPyKiiU4ogvqIbkiaP0QEvzdhSM5BgV6nPPVQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE0/ODE2Mjc4MzQ4NzYt/Yjc4MzNlOGY1NTcw/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZpeGxp/Yj1yYi00LjAuMyZp/eGlkPU0zd3hNakEz/ZkRCOE1IeHpaV0Z5/WTJoOE1ueDhiR2xp/Y21GeWVYeGxibnd3/Zkh3d2ZIeDhNQT09.jpeg",
+  },
+  {
+    name: "Food",
+    imageUrl:
+      "https://imgs.search.brave.com/CR2Dv1qD-Jo_YdHMjLwloJdTxrvGzsm7lPdHMfGvDGA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTU2/NTYxOTY1L3Bob3Rv/L3dvbWFuLWhhdmlu/Zy1mb29kLWF0LXJl/c3RhdXJhbnQtdGFi/bGUuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPW1UdDNuUzVE/M0t0XzNaMzROMHVi/LU1qOXg0Q2xva0R0/U2JKcXhQNndWZUE9",
+  },
+  {
+    name: "Fitness",
+    imageUrl:
+      "https://imgs.search.brave.com/FJh8DEQHe9LkqhfNqHma9cYeaPUrtT14L43qtbJLedI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3F1YXJlc3Bh/Y2UtY2RuLmNvbS9j/b250ZW50L3YxLzU3/OWQxMDU2ZjdlMGFi/ZjhkODllMWY3ZC8x/NzAxMTE3NTc5MjQ2/LTRUM1FIUFBYREpN/ME9EV0pYNkdBL1JB/SUxZQVJEK0ZJVE5F/U1MrMjMtMDAzLmpw/Zw",
+  },
+  {
+    name: "Travel",
+    imageUrl:
+      "https://imgs.search.brave.com/7VPPCBtwcs-mvif9tPxlltPQFgOo_KuU-2f3UplT0AY/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90cmF2/ZWxpc2xpZmUub3Jn/L3dwLWNvbnRlbnQv/dXBsb2Fkcy9TaHV0/dGVyb2dyYXBoeS5q/cGc",
+    },
+  {
+    name: "Movie",
+    imageUrl:
+      "https://imgs.search.brave.com/Xd-msBMsBb_mb333XOesx7znpsi_iahlokskcYyiURQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxOS8x/MS8wNy8yMC80OC9j/aW5lbWEtNDYwOTg3/N182NDAuanBn",
+  },
+  {
+    name: "Education",
+    imageUrl:
+      "https://imgs.search.brave.com/j-DsRwzD7uXtDtIQPbnRQZVQgiFt5JOjDGnyimQJf4I/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNS8w/OS8wNS8yMS81MS9y/ZWFkaW5nLTkyNTU4/OV82NDAuanBn",
+  },
+];
+
+export default filters;
